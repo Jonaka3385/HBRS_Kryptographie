1fc7f08a448cee93a5d52b347920fd0a141a3aaf
Jonaka <jonakactfs@gmail.com>
2023-04-08 15:43:33 +0200

    ak2 prak1a1 Schlüsselgenerator fertig

---
+++ ak2/prak1a1.py
@@ -0,0 +1,56 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+
+
+def generate_key(bit_size, public_exponent):
+    private_key = rsa.generate_private_key(
+        public_exponent=public_exponent,
+        key_size=bit_size,
+        backend=default_backend()
+    )
+
+    # privater Schlüssel PEM-kodiert als String
+    private_key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ).decode()
+
+    # öffentlicher Schlüssel PEM-kodiert als String
+    public_key_pem = private_key.public_key().public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ).decode()
+
+    path_priv = "/Users/jonas/Documents/JetBrains_Projects/PyCharm/Kryptographie/ak2/prak1Files/priv_key.pem"
+    path_pub = "/Users/jonas/Documents/JetBrains_Projects/PyCharm/Kryptographie/ak2/prak1Files/pub_key.pem"
+    # privaten Schlüssel als PEM-Datei schreiben
+    with open(path_priv, "wb") as f:
+        f.write(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        ))
+
+    # öffentlichen Schlüssel als PEM-Datei schreiben
+    with open(path_pub, "wb") as f:
+        f.write(private_key.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ))
+
+    return private_key_pem, public_key_pem
+
+
+def start():
+    bit_size = 3000
+    exp = 65537  # 2^16 + 1
+    priv_key, pub_key = generate_key(bit_size, exp)
+    print("Key: ")
+    print(priv_key)
+    print()
+    print(pub_key)
+    print()
+    print(f"Bitlänge: {bit_size}")
+    print(f"Öffentlicher Exponent: {exp}")
--- allgemein/main.py
+++ allgemein/main.py
@@ -1,7 +1,9 @@
 from ak1 import prak1a1 as p111
 from ak1 import prak1a2 as p112
+from ak2 import prak1a1 as p211
 
 
 if __name__ == '__main__':
-    p111.quickstart()
-    p112.quickstart()
+    # p111.quickstart()
+    # p112.quickstart()
+    p211.start()

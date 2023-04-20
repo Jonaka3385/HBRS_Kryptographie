b3448f85d0be0db89e644e27d98e2c708a280dea
Jonaka <jonakactfs@gmail.com>
2023-04-20 11:21:03 +0200

    b

--- allgemein/main.py
+++ allgemein/main.py
@@ -1,11 +1,7 @@
 from ak1 import prak1a1 as p111
 from ak1 import prak1a2 as p112
-from ak2 import prak1a1 as p211
-from ak2 import prak1a2 as p212
 
 
 if __name__ == '__main__':
     p111.quickstart()
     p112.quickstart()
-    # p211.quickstart()
-    # p212.start()

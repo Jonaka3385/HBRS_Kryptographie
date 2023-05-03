2a298ea8e7586e16727f6aeacb5d5a8951618c09
Jonaka <jonakactfs@gmail.com>
2023-05-03 13:25:10 +0200

    ak1/prak2a2 fertig aber mit sehr schlechter Laufzeit

--- ak1/prak2a2.py
+++ ak1/prak2a2.py
@@ -2,6 +2,20 @@ from itertools import product
 import string
 
 
+def same_length(a, length):
+    if len(a) < length:
+        # füge Leerzeichen am Ende hinzu, bis die gewünschte Länge erreicht ist
+        copied_string = (a * (length // len(a) + 1))[:length]
+    elif len(a) > length:
+        # schneide den String ab, bis die gewünschte Länge erreicht ist
+        copied_string = a[:length]
+    else:
+        # der String hat bereits die gewünschte Länge
+        copied_string = a
+
+    return copied_string # Ausgabe: 'ABC    '
+
+
 def is_ascii_bytes(c):
     try:
         p = c.decode('ascii')
@@ -28,32 +42,35 @@ def generate_bytearrays(length):
 
 
 def decrypt(cipher0, cipher1, cipher2):
-    key_length = 1
-    while key_length < 20:
+    key_length = 7
+    cipher_length = len(cipher0)
+    while key_length < 10:
+        print(f'Trying length: {key_length}')
         letters = string.ascii_uppercase
-        combinations_list = list(product(letters, repeat=key_length))
-        for combination in combinations_list:
+        # combinations_list = list(product(letters, repeat=key_length))
+        for combination in product(letters, repeat=key_length):
             str_key = ''.join(combination)
+            str_key = same_length(str_key, cipher_length)
             key = str_key.encode('ascii')
-            p0 = bytearray(key_length)
-            for r in range(key_length):
+            p0 = bytearray(cipher_length)
+            for r in range(cipher_length):
                 p0[r] = cipher0[r] ^ key[r]
-                if is_valid(p0.decode('ascii')):
-                    p1 = bytearray(key_length)
-                    for s in range(key_length):
-                        p1[s] = cipher1[s] ^ key[s]
-                        if is_valid(p1.decode('ascii')):
-                            p2 = bytearray(key_length)
-                            for t in range(key_length):
-                                p2[t] = cipher2[t] ^ key[t]
-                                if is_valid(p2.decode('ascii')):
-                                    print(f"p0: {p0},\n"
-                                          f"p0 decoded: {p0.decode('ascii')},\n\n"
-                                          f"p1: {p1},\n"
-                                          f"p1 decoded: {p1.decode('ascii')},\n\n"
-                                          f"p2: {p2},\n"
-                                          f"p2 decoded: {p2.decode('ascii')},\n\n"
-                                          f"key: {key}\n\n\n")
+            if is_valid(p0.decode('ascii')):
+                p1 = bytearray(cipher_length)
+                for s in range(cipher_length):
+                    p1[s] = cipher1[s] ^ key[s]
+                if is_valid(p1.decode('ascii')):
+                    p2 = bytearray(cipher_length)
+                    for t in range(cipher_length):
+                        p2[t] = cipher2[t] ^ key[t]
+                    if is_valid(p2.decode('ascii')):
+                        print(f"p0: {p0},\n"
+                                f"p0 decoded: {p0.decode('ascii')},\n\n"
+                                f"p1: {p1},\n"
+                                f"p1 decoded: {p1.decode('ascii')},\n\n"
+                                f"p2: {p2},\n"
+                                f"p2 decoded: {p2.decode('ascii')},\n\n"
+                                f"key: {key}\n\n\n")
         key_length += 1
     print("Keine Ahnung")
 

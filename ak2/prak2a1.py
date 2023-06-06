1fe2db00afd13e11081e732053d1a5f8d8be91e6
Jonaka <jonakactfs@gmail.com>
2023-06-06 17:28:18 +0200

    ak2prak2 fertig, hoffentlich richtig

--- ak2/prak2a1.py
+++ ak2/prak2a1.py
@@ -1,4 +1,5 @@
-import random
+from random import randint
+from time import time
 
 
 def gcd(gcd_a, gcd_b):
@@ -54,7 +55,7 @@ def rsa_decrypt(re_cipher, re_d, re_n):
     return re_msg
 
 
-def qft(qft_n, qft_a):
+def qft(qft_a, qft_n):
     qft_tmp = pow(qft_a, 1, qft_n)
     qft_i = 2
     while not pow(qft_a, qft_i, qft_n) == qft_tmp:
@@ -62,32 +63,35 @@ def qft(qft_n, qft_a):
     return qft_i-1
 
 
-def method():
-    p = random.randint(100, 200)
+def shor():
+    p = randint(100, 200)
     while not isPrime(p):
-        p = random.randint(100, 200)
-    q = random.randint(100, 200)
+        p = randint(100, 200)
+    q = randint(100, 200)
     while not isPrime(q) and not p == q:
-        q = random.randint(100, 200)
+        q = randint(100, 200)
     n = p*q
-    print(f'p: {p}', f'q: {q}', f'n: {n}', '', sep='\n')
+    print('\n\n', f'p: {p}', f'q: {q}', f'n: {n}', '', sep='\n')
     a = 2
-    r = qft(n, a)
+    r = qft(a, n)
+    while a < n and not r % 2 == 0:
+        a += 1
+        r = qft(a, n)
     print(f'r: {r}', sep='\n')
     if r % 2 == 0:
         x = pow(2, int(r/2), n)
-        print(f'x: {x}', '', sep='\n')
+        print(f'x: {x}', f'a: {a}', '', sep='\n')
     else:
-        x = 0
+        return False, False
     p_tmp = gcd(x-1, n)
     q_tmp = gcd(x+1, n)
     print(f'p_tmp: {p_tmp}', f'q_tmp: {q_tmp}', sep='\n')
     if not p_tmp * q_tmp == n:
-        return False
+        return False, False
     if not isPrime(p_tmp):
-        return False
+        return False, False
     if not isPrime(q_tmp):
-        return False
+        return False, False
     if (p_tmp == p or p_tmp == q) or (q_tmp == p or q_tmp == q):
         print('Success?\n')
 
@@ -99,14 +103,24 @@ def method():
     print()
     crypt_worked = msg == new_msg
     print(msg, cipher, new_msg, f'Crypt worked: {crypt_worked}', sep='\n')
-    return crypt_worked
+    return True, crypt_worked
 
 
 if __name__ == '__main__':
+    start = time()
     worked = 0
-    tries = 1000
+    tries = 100
+    crypt_worked = 0
     for i in range(tries - 1):
-        if method():
+        sw, cw = shor()
+        if sw:
             worked += 1
+        if cw == 1:
+            crypt_worked += 1
+
+    end = time()
+    print(f'\n\nTime passed:          {end - start}s')
     worked_pro = worked / (tries/100)
-    print('\n\n', f'worked: {worked} out of {tries}', f'Prozent worked: {worked_pro}', sep='\n')
+    print(f'worked:               {worked} out of {tries}; {worked_pro}%')
+    crypt_worked_pro = crypt_worked / (tries / 100)
+    print(f'worked with crypting: {crypt_worked} out of {tries}; {crypt_worked_pro}%')

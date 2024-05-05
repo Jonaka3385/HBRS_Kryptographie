ebc9576d352c462bbd8fac0c485eb39c92c671ae
Jonas Kawohl <j.kawohl@icloud.com>
2024-05-05 18:30:50 +0200

    ak2_ecc_u2

--- ak2/ecc/uebung2.py
+++ ak2/ecc/uebung2.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass
 from gmpy2 import mpz
 
 gf = 0
+a = 0
+b = 0
 
 
 @dataclass
@@ -34,11 +36,11 @@ def on_courve(point):
     if isinstance(point, PointXY):
         # y^2 = x^3 + 16x + 7 mod GF(31)
         sum1 = (point.y ** 2) % gf
-        sum2 = ((point.x ** 3) + (16 * point.x) + 7) % gf
+        sum2 = ((point.x ** 3) + (a * point.x) + b) % gf
     elif isinstance(point, PointXYZ):
         # y^2*z = x^3 + 16x*z^2 + 7*z^3 mod GF(31)
         sum1 = ((point.y ** 2) * point.z) % gf
-        sum2 = ((point.x ** 3) + ((16 * point.x) * (point.z ** 2)) + (7 * (point.z ** 3))) % gf
+        sum2 = ((point.x ** 3) + ((a * point.x) * (point.z ** 2)) + (b * (point.z ** 3))) % gf
     else:
         return False
     return sum1 == sum2
@@ -69,30 +71,63 @@ def is_congruent(point1, point2):
     return x1 == x2 and y1 == y2
 
 
+def point_add(point1, point2):
+    if isinstance(point1, PointXY) and isinstance(point2, PointXY):
+        x1 = point1.x
+        y1 = point1.y
+        x2 = point2.x
+        y2 = point2.y
+        if x1 == x2 or x1 == -x2:
+            return point_double(point1)
+        else:
+            s = ((y2 - y1) / (x2 - x1)) % gf
+            x3 = ((s ** 2) - x1 - x2) % gf
+            y3 = ((s * (x1 - x3)) - y1) % gf
+            point3 = PointXY(mpz(x3), mpz(y3))
+            return point3
+    else:
+        raise Exception("Points must be of type PointXY")
+
+
+def point_double(point1):
+    if point1.x == 0:
+        raise Exception("Points must be of type PointXY")
+    else:
+        x1 = point1.x
+        y1 = point1.y
+        s = ((3 * (x1 ** 2) + a) / (2 * y1)) % gf
+        x3 = ((s ** 2) - (2 * x1)) % gf
+        y3 = ((s * (x1 - x3)) - y1) % gf
+        point3 = PointXY(mpz(x3), mpz(y3))
+        return point3
+
+
 if __name__ == '__main__':
     gf = 31
+    a = 16
+    b = 7
 
     # 2.1
-    a = PointXY(mpz(28), mpz(26))
-    b = PointXY(mpz(28), mpz(5))
-    c = PointXY(mpz(121), mpz(-5))
-    d = PointXY(mpz(0), mpz(0))
-    e = PointXYZ(mpz(0), mpz(1), mpz(0))
-    f = PointXYZ(mpz(28), mpz(5), mpz(1))
-    g = PointXY(mpz(0), mpz(10))
-    h = PointXYZ(mpz(56), mpz(52), mpz(2))
-    s_a = on_courve(a)
-    s_b = on_courve(b)
-    s_c = on_courve(c)
-    s_d = on_courve(d)
-    s_e = on_courve(e)
-    s_f = on_courve(f)
-    s_g = on_courve(g)
-    s_h = on_courve(h)
+    p_a = PointXY(mpz(28), mpz(26))
+    p_b = PointXY(mpz(28), mpz(5))
+    p_c = PointXY(mpz(121), mpz(-5))
+    p_d = PointXY(mpz(0), mpz(0))
+    p_e = PointXYZ(mpz(0), mpz(1), mpz(0))
+    p_f = PointXYZ(mpz(28), mpz(5), mpz(1))
+    p_g = PointXY(mpz(0), mpz(10))
+    p_h = PointXYZ(mpz(56), mpz(52), mpz(2))
+    s_a = on_courve(p_a)
+    s_b = on_courve(p_b)
+    s_c = on_courve(p_c)
+    s_d = on_courve(p_d)
+    s_e = on_courve(p_e)
+    s_f = on_courve(p_f)
+    s_g = on_courve(p_g)
+    s_h = on_courve(p_h)
     print(s_a, s_b, s_c, s_d, s_e, s_f, s_g, s_h, '', sep='\n')
-    a_c_con = is_congruent(a, c)
-    a_h_con = is_congruent(c, h)
-    b_f_con = is_congruent(b, f)
+    a_c_con = is_congruent(p_a, p_c)
+    a_h_con = is_congruent(p_c, p_h)
+    b_f_con = is_congruent(p_b, p_f)
     print('congruent:', f'a zu c: {a_c_con}', f'a zu h: {a_h_con}', f'b zu f: {b_f_con}', sep='\n')
 
     # 2.2

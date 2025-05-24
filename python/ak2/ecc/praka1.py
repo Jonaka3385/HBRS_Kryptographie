7540b1724dc15d9405726b58747bf2a60ae30c84
Jonas Kawohl <j.kawohl@icloud.com>
2025-05-24 18:50:57 +0200

    update

--- python/ak2/ecc/praka1.py
+++ python/ak2/ecc/praka1.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 
 
+"""
+Affiner Punkt
+"""
 @dataclass
 class PointXY:
     x: int
@@ -39,6 +42,17 @@ class PointXY:
 
             return PointXY(x3, y3)
 
+    def __mul__(self, k: int):
+        q = xy_neutral
+        kb = bin(k)
+        # k = 2  ->  kb = '0b10'
+        j = len(kb)
+        for i in range(2, j, +1):
+            q = q.dbl()
+            if kb[i] == '1':
+                q = q + self
+        return q
+
     def dbl(self):
         if self == xy_neutral:
             return xy_neutral
@@ -56,6 +70,9 @@ class PointXY:
     def to_xyz(self):
         return PointXYZ(self.x, self.y, 1)
 
+"""
+Projektiver Punkt
+"""
 @dataclass
 class PointXYZ:
     x: int
@@ -100,6 +117,17 @@ class PointXYZ:
 
             return PointXYZ(x3, y3, z3)
 
+    def __mul__(self, k: int):
+        q = xyz_neutral
+        kb = bin(k)
+        # k = 2  ->  kb = '0b10'
+        j = len(kb)
+        for i in range(2, j, +1):
+            q = q.dbl()
+            if kb[i] == '1':
+                q = q + self
+        return q
+
     def dbl(self):
         if self == xyz_neutral:
             return xyz_neutral
@@ -127,6 +155,9 @@ class PointXYZ:
             new_y = int(self.y / self.z)
             return PointXY(new_x, new_y)
 
+    def __str__(self):
+        return f'({self.x}, {self.y}, {self.z})'
+
 xyz_neutral = PointXYZ(0, 1, 0)
 xy_neutral = PointXY(None, None)
 
@@ -134,10 +165,61 @@ gf = 0
 a = 0
 b = 0
 
+def on_short_weierstrass(point: PointXY):
+    x = point.x
+    y = point.y
+
+    left = (y**2) % gf
+    right = (x**3 + a*x + b) % gf
+
+    return left == right
+
+def on_homogenised_weierstrass(point: PointXYZ):
+    x = point.x
+    y = point.y
+    z = point.z
+
+    left = (y**2 * z) % gf
+    right = (x**3 + a * x * z**2 + b * z**3) % gf
+
+    return left == right
+
+def skalarmul(point: PointXY, k):
+    point = point.to_xyz()
+    q = xyz_neutral
+    j = len(k)-1
+    for i in range(j, -1, -1):
+        q = q.dbl()
+        if k[i] == '1':
+            q = q + point
+    return q.to_xy()
+
+
+"""
+main() Methode
+"""
 if __name__ == "__main__":
     # Y^2*Z = X^3 + a*X*Z^2 + b*Z^3
     # y^2 = x^3 + a*X + b
-    gf = 31
-    a = 16
-    b = 7
-    print('')
+    """
+    gf = int(0xA9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377)
+    a = int(0x7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9)
+    b = int(0x26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6)
+    p = PointXYZ(60306380415904663168568911239273826053144841234228559299517684417361346433053,
+                 74653857005150983469598545140707432309023702960881435319026826228339031179596, 1)
+    p_val = on_homogenised_weierstrass(p)
+    p2 = p.dbl()
+    p3 = p * 9
+    pq = p + p2
+    print(f'p: {p}, valid: {p_val}', f'p2 {p2}', f'p3 {p3}', f'pq {pq}', sep='\n')
+    """
+    gf = int(0xA9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377)
+    a = int(0x7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9)
+    b = int(0x26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6)
+    p = PointXY(60306380415904663168568911239273826053144841234228559299517684417361346433053,
+                 74653857005150983469598545140707432309023702960881435319026826228339031179596)
+    p_val = on_short_weierstrass(p)
+    p2 = p.dbl()
+    p3 = p * 9
+    pq = p + p2
+    print(f'p: {p}, valid: {p_val}', f'p2 {p2}', f'p3 {p3}', f'pq {pq}', sep='\n')

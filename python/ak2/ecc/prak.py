1926b9b0d8980b9a227298878cbc0dae99554cd9
Jonas Kawohl <j.kawohl@icloud.com>
2025-05-29 17:01:50 +0200

    update

--- python/ak2/ecc/prak.py
+++ python/ak2/ecc/prak.py
@@ -1,8 +1,23 @@
 from dataclasses import dataclass
 
+"""
+Elliptische Kurve - Brainpool P-256
+Korrigierte Version des ursprünglichen Codes
+"""
+
+p = int(0xA9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377)
+a = int(0x7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9)
+b = int(0x26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6)
+
+
+def mod_inv(k, mod):
+    if k == 0:
+        raise ZeroDivisionError('Division durch Null')
+    return pow(k, -1, mod)
+
 
 """
-Affiner Punkt
+Affiner Punkt - KORRIGIERT
 """
 @dataclass
 class PointXY:
@@ -16,11 +31,12 @@ class PointXY:
             return False
 
     def __neg__(self):
-        return PointXY(self.x, -self.y)
+        return PointXY(self.x, (-self.y) % p)  # Modulo hinzugefügt
 
     def __add__(self, other):
         if not isinstance(other, PointXY):
             raise Exception("Point must be of type PointXY")
+
         if self == xy_neutral:
             return other
         elif other == xy_neutral:
@@ -30,12 +46,10 @@ class PointXY:
         elif self == -other:
             return xy_neutral
         else:
-            x1 = self.x
-            x2 = other.x
-            y1 = self.y
-            y2 = other.y
+            x1, y1 = self.x, self.y
+            x2, y2 = other.x, other.y
 
-            s = int(((y2 - y1) * mod_inv(x2 - x1, p)) % p)
+            s = ((y2 - y1) * mod_inv((x2 - x1) % p, p)) % p
 
             x3 = ((s ** 2) - x1 - x2) % p
             y3 = ((s * (x1 - x3)) - y1) % p
@@ -43,15 +57,22 @@ class PointXY:
             return PointXY(x3, y3)
 
     def __mul__(self, k: int):
-        q = xy_neutral
-        kb = bin(k)
-        # k = 2  ->  kb = '0b10'
-        j = len(kb)
-        for i in range(2, j, +1):
-            q = q.dbl()
-            if kb[i] == '1':
-                q = q + self
-        return q
+        # KORRIGIERTE Skalarmultiplikation
+        if k == 0:
+            return xy_neutral
+        if k < 0:
+            return (-self) * (-k)
+
+        result = xy_neutral
+        addend = self
+
+        while k:
+            if k & 1:
+                result = result + addend
+            addend = addend.dbl()
+            k >>= 1
+
+        return result
 
     def __str__(self):
         return f'({self.x}, {self.y})'
@@ -60,21 +81,23 @@ class PointXY:
         if self == xy_neutral or self.y == 0:
             return xy_neutral
         else:
-            x1 = self.x
-            y1 = self.y
+            x1, y1 = self.x, self.y
 
-            s = int(((3 * (x1 ** 2) + a) * mod_inv(2 * y1, p)) % p)
+            s = ((3 * (x1 ** 2) + a) * mod_inv((2 * y1) % p, p)) % p
 
-            x3 = ((s**2) - (2 * x1)) % p
+            x3 = ((s ** 2) - (2 * x1)) % p
             y3 = ((s * (x1 - x3)) - y1) % p
 
             return PointXY(x3, y3)
 
     def to_xyz(self):
+        if self == xy_neutral:
+            return xyz_neutral
         return PointXYZ(self.x, self.y, 1)
 
+
 """
-Projektiver Punkt
+Projektiver Punkt - KORRIGIERT
 """
 @dataclass
 class PointXYZ:
@@ -83,17 +106,19 @@ class PointXYZ:
     z: int
 
     def __eq__(self, other):
-        if self.x == other.x and self.y == other.y and self.z == other.z:
-            return True
-        else:
-            return False
+        if self.z == 0 and other.z == 0:
+            return True  # Beide sind Punkte im Unendlichen
+        if self.z == 0 or other.z == 0:
+            return False  # Einer ist im Unendlichen, der andere nicht
+        return (self.x * other.z) % p == (other.x * self.z) % p and (self.y * other.z) % p == (other.y * self.z) % p
 
     def __neg__(self):
-        return PointXYZ(self.x, -self.y, self.z)
+        return PointXYZ(self.x, (-self.y) % p, self.z)
 
     def __add__(self, other):
         if not isinstance(other, PointXYZ):
             raise Exception("Point must be of type PointXYZ")
+
         if self == xyz_neutral:
             return other
         elif other == xyz_neutral:
@@ -103,33 +128,56 @@ class PointXYZ:
         elif self == -other:
             return xyz_neutral
         else:
-            x1 = self.x
-            x2 = other.x
-            y1 = self.y
-            y2 = other.y
-            z1 = self.z
-            z2 = other.z
+            x1, y1, z1 = self.x, self.y, self.z
+            x2, y2, z2 = other.x, other.y, other.z
 
-            b_a = (y2 * z1 - y1 * z2) % p
-            b_b = (x2 * z1 - x1 * z2) % p
-            b_c = (b_a ** 2 * z1 * z2 - b_b ** 3 - 2 * b_b ** 2 * x1 * z2) % p
+            # Berechne u1, u2, s1, s2
+            u1 = (x1 * z2) % p
+            u2 = (x2 * z1) % p
+            s1 = (y1 * z2) % p
+            s2 = (y2 * z1) % p
 
-            x3 = (b_b * b_c) % p
-            y3 = (b_a * (b_b ** 2 * x1 * z2 - b_c) - b_b ** 3 * y1 * z2) % p
-            z3 = (b_b ** 3 * z1 * z2) % p
+            # Wenn u1 == u2, dann sind die x-Koordinaten gleich
+            if u1 == u2:
+                if s1 == s2:
+                    # Gleiche Punkte - Verdopplung
+                    return self.dbl()
+                else:
+                    # Inverse Punkte - Ergebnis ist neutrales Element
+                    return xyz_neutral
+
+            # Berechne h und r
+            h = (u2 - u1) % p
+            r = (s2 - s1) % p
+
+            # Berechne neue Koordinaten
+            h2 = (h * h) % p
+            h3 = (h2 * h) % p
+            u1h2 = (u1 * h2) % p
+
+            x3 = (r * r - h3 - 2 * u1h2) % p
+            y3 = (r * (u1h2 - x3) - s1 * h3) % p
+            z3 = (z1 * z2 * h) % p
 
             return PointXYZ(x3, y3, z3)
 
     def __mul__(self, k: int):
-        q = xyz_neutral
-        kb = bin(k)
-        # k = 2  ->  kb = '0b10'
-        j = len(kb)
-        for i in range(2, j, +1):
-            q = q.dbl()
-            if kb[i] == '1':
-                q = q + self
-        return q
+        # KORRIGIERTE Skalarmultiplikation
+        if k == 0:
+            return xyz_neutral
+        if k < 0:
+            return (-self) * (-k)
+
+        result = xyz_neutral
+        addend = self
+
+        while k:
+            if k & 1:
+                result = result + addend
+            addend = addend.dbl()
+            k >>= 1
+
+        return result
 
     def __str__(self):
         return f'({self.x}, {self.y}, {self.z})'
@@ -138,18 +186,24 @@ class PointXYZ:
         if self == xyz_neutral:
             return xyz_neutral
         else:
-            x1 = self.x
-            y1 = self.y
-            z1 = self.z
+            x1, y1, z1 = self.x, self.y, self.z
 
-            b_a = (a * z1 ** 2 + 3 * x1 ** 2) % p
-            b_b = (y1 * z1) % p
-            b_c = (x1 * y1 * b_b) % p
-            b_d = (b_a ** 2 - 8 * b_c) % p
+            # Spezialfall: y1 = 0 (Punkt hat Ordnung 2)
+            if y1 == 0:
+                return xyz_neutral
 
-            x3 = (2 * b_b * b_d) % p
-            y3 = (b_a * (4 * b_c - b_d) - 8 * y1 ** 2 * b_b ** 2) % p
-            z3 = (8 * b_b ** 3) % p
+            # KORRIGIERTE projektive Verdopplung (Jacobi-Koordinaten)
+            y1_2 = (y1 * y1) % p
+            y1_4 = (y1_2 * y1_2) % p
+            x1_2 = (x1 * x1) % p
+            z1_2 = (z1 * z1) % p
+
+            s = (4 * x1 * y1_2) % p
+            m = (3 * x1_2 + a * z1_2) % p
+
+            x3 = (m * m - 2 * s) % p
+            y3 = (m * (s - x3) - 8 * y1_4) % p
+            z3 = (2 * y1 * z1) % p
 
             return PointXYZ(x3, y3, z3)
 
@@ -158,88 +212,90 @@ class PointXYZ:
             return xy_neutral
         else:
             z_inv = mod_inv(self.z, p)
-            new_x = self.x * z_inv
-            new_y = self.y * z_inv
+            new_x = (self.x * z_inv) % p
+            new_y = (self.y * z_inv) % p
             return PointXY(new_x, new_y)
 
 
+# KORRIGIERTE neutrale Elemente
 xyz_neutral = PointXYZ(0, 1, 0)
-xy_neutral = PointXY(None, None)
+xy_neutral = PointXY(0, 0)  # KORRIGIERT: Verwende (0,0) statt (None, None)
 
-p = 0
-a = 0
-b = 0
 
+# KORRIGIERTE Validierungsfunktionen
 def on_short_weierstrass(point: PointXY):
+    # Sonderbehandlung für neutrales Element
+    if point == xy_neutral:
+        return True
+
     x1 = point.x
     y1 = point.y
 
-    left = (y1**2) % p
-    right = (x1**3 + a*x1 + b) % p
+    left = (y1 ** 2) % p
+    right = (x1 ** 3 + a * x1 + b) % p
 
     return left == right
 
+
 def on_homogenised_weierstrass(point: PointXYZ):
+    # Sonderbehandlung für neutrales Element
+    if point.z == 0:
+        return True
+
     x1 = point.x
     y1 = point.y
     z1 = point.z
 
-    left = (y1**2 * z1) % p
-    right = (x1**3 + a * x1 * z1**2 + b * z1**3) % p
+    left = (y1 ** 2 * z1) % p
+    right = (x1 ** 3 + a * x1 * z1 ** 2 + b * z1 ** 3) % p
 
     return left == right
+
 
 def skalarmul(point: PointXY, k):
     point = point.to_xyz()
     q = xyz_neutral
-    j = len(k)-1
+    j = len(k) - 1
     for i in range(j, -1, -1):
         q = q.dbl()
         if k[i] == '1':
             q = q + point
     return q.to_xy()
 
-def mod_inv(k, mod):
-    if k == 0:
-        raise ZeroDivisionError('Division durch Null')
-    return pow(k, -1, mod)
-
 
 """
-main() Methode
+main() Methode - KORRIGIERT
 """
 if __name__ == "__main__":
-    # Y^2*Z = X^3 + a*X*Z^2 + b*Z^3
-    # y^2 = x^3 + a*X + b
-    p = int(0xA9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377)
-    a = int(0x7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9)
-    b = int(0x26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6)
+    print("=== KORRIGIERTE VERSION ===")
 
+    # Test mit projektiven Punkten
     point_p = PointXYZ(60306380415904663168568911239273826053144841234228559299517684417361346433053,
-                 74653857005150983469598545140707432309023702960881435319026826228339031179596, 1)
+                       74653857005150983469598545140707432309023702960881435319026826228339031179596, 1)
     p_val = on_homogenised_weierstrass(point_p)
     p2 = point_p.dbl()
     p2_val = on_homogenised_weierstrass(p2)
-    p3 = point_p * 9
+    p3 = point_p + p2
     p3_val = on_homogenised_weierstrass(p3)
-    pq = point_p + p2
-    pq_val = on_homogenised_weierstrass(pq)
-    print(f'point: {point_p}, valid: {p_val}', f'p2 {p2}, valid: {p2_val}', f'p3 {p3}, valid: {p3_val}',
-          f'pq {pq}, valid: {pq_val}', f'', sep='\n')
+    print(f'point: {point_p}, valid: {p_val}')
+    print(f'p2 {p2}, valid: {p2_val}')
+    print(f'p3 {p3}, valid: {p3_val}')
+    print()
 
+    # Test mit affinen Punkten
     point_p = PointXY(60306380415904663168568911239273826053144841234228559299517684417361346433053,
-                 74653857005150983469598545140707432309023702960881435319026826228339031179596)
+                      74653857005150983469598545140707432309023702960881435319026826228339031179596)
     p_val = on_short_weierstrass(point_p)
     p2 = point_p.dbl()
     p2_val = on_short_weierstrass(p2)
-    p3 = point_p * 9
-    p3_val = on_short_weierstrass(p3)
     pq = point_p + p2
     pq_val = on_short_weierstrass(pq)
-    print(f'point: {point_p}, valid: {p_val}', f'p2 {p2}, valid: {p2_val}', f'p3 {p3}, valid: {p3_val}',
-          f'pq {pq}, valid: {pq_val}', f'', sep='\n')
+    print(f'point: {point_p}, valid: {p_val}')
+    print(f'p2 {p2}, valid: {p2_val}')
+    print(f'pq {pq}, valid: {pq_val}')
+    print()
 
-    #DH
+    # Diffie-Hellman Test
     Gf = PointXY(63243729749562333355292243550312970334778175571054726587095381623627144114786,
                  38218615093753523893122277964030810387585405539772602581557831887485717997975)
     x = 13
@@ -248,7 +304,9 @@ if __name__ == "__main__":
     yG = Gf * y
     y_xG = xG * y
     x_yG = yG * x
-    print(f'Gf: {Gf}, {on_short_weierstrass(Gf)}',
-          f'xG: {xG}, {on_short_weierstrass(xG)}', f'yG: {yG}, {on_short_weierstrass(yG)}',
-          f'y_xG: {y_xG}, {on_short_weierstrass(y_xG)}', f'x_yG: {x_yG}, {on_short_weierstrass(x_yG)}',
-          f'{y_xG == x_yG}', f'xyG == yxG: {y_xG == x_yG}', sep='\n')
+    print(f'Gf: {Gf}, {on_short_weierstrass(Gf)}')
+    print(f'xG: {xG}, {on_short_weierstrass(xG)}')
+    print(f'yG: {yG}, {on_short_weierstrass(yG)}')
+    print(f'y_xG: {y_xG}, {on_short_weierstrass(y_xG)}')
+    print(f'x_yG: {x_yG}, {on_short_weierstrass(x_yG)}')
+    print(f'xyG == yxG: {y_xG == x_yG}')
---
+++ python/ak2/ecc/prak_v0.py
@@ -0,0 +1,248 @@
+from dataclasses import dataclass
+
+
+"""
+Affiner Punkt
+"""
+@dataclass
+class PointXY:
+    x: int
+    y: int
+
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y:
+            return True
+        else:
+            return False
+
+    def __neg__(self):
+        return PointXY(self.x, (-self.y) % p)
+
+    def __add__(self, other):
+        if not isinstance(other, PointXY):
+            raise Exception("Point must be of type PointXY")
+
+        if self == xy_neutral:
+            return other
+        elif other == xy_neutral:
+            return self
+        elif self == other:
+            return self.dbl()
+        elif self == -other:
+            return xy_neutral
+        else:
+            x1, y1 = self.x, self.y
+            x2, y2 = other.x, other.y
+
+            s = ((y2 - y1) * mod_inv((x2 - x1) % p, p)) % p
+
+            x3 = ((s ** 2) - x1 - x2) % p
+            y3 = ((s * (x1 - x3)) - y1) % p
+
+            return PointXY(x3, y3)
+
+    def __mul__(self, k: int):
+        q = xy_neutral
+        kb = bin(k)
+        # k = 2  ->  kb = '0b10'
+        j = len(kb)
+        for i in range(2, j, +1):
+            q = q.dbl()
+            if kb[i] == '1':
+                q = q + self
+        return q
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def dbl(self):
+        if self == xy_neutral or self.y == 0:
+            return xy_neutral
+        else:
+            x1, y1 = self.x, self.y
+
+            s = ((3 * (x1 ** 2) + a) * mod_inv((2 * y1) % p, p)) % p
+
+            x3 = ((s ** 2) - (2 * x1)) % p
+            y3 = ((s * (x1 - x3)) - y1) % p
+
+            return PointXY(x3, y3)
+
+    def to_xyz(self):
+        if self == xy_neutral:
+            return xyz_neutral
+        return PointXYZ(self.x, self.y, 1)
+
+"""
+Projektiver Punkt
+"""
+@dataclass
+class PointXYZ:
+    x: int
+    y: int
+    z: int
+
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y and self.z == other.z:
+            return True
+        else:
+            return False
+
+    def __neg__(self):
+        return PointXYZ(self.x, -self.y, self.z)
+
+    def __add__(self, other):
+        if not isinstance(other, PointXYZ):
+            raise Exception("Point must be of type PointXYZ")
+        if self == xyz_neutral:
+            return other
+        elif other == xyz_neutral:
+            return self
+        elif self == other:
+            return self.dbl()
+        elif self == -other:
+            return xyz_neutral
+        else:
+            x1 = self.x
+            x2 = other.x
+            y1 = self.y
+            y2 = other.y
+            z1 = self.z
+            z2 = other.z
+
+            b_a = (y2 * z1 - y1 * z2) % p
+            b_b = (x2 * z1 - x1 * z2) % p
+            b_c = (b_a ** 2 * z1 * z2 - b_b ** 3 - 2 * b_b ** 2 * x1 * z2) % p
+
+            x3 = (b_b * b_c) % p
+            y3 = (b_a * (b_b ** 2 * x1 * z2 - b_c) - b_b ** 3 * y1 * z2) % p
+            z3 = (b_b ** 3 * z1 * z2) % p
+
+            return PointXYZ(x3, y3, z3)
+
+    def __mul__(self, k: int):
+        q = xyz_neutral
+        kb = bin(k)
+        # k = 2  ->  kb = '0b10'
+        j = len(kb)
+        for i in range(2, j, +1):
+            q = q.dbl()
+            if kb[i] == '1':
+                q = q + self
+        return q
+
+    def __str__(self):
+        return f'({self.x}, {self.y}, {self.z})'
+
+    def dbl(self):
+        if self == xyz_neutral:
+            return xyz_neutral
+        else:
+            x1 = self.x
+            y1 = self.y
+            z1 = self.z
+
+            b_a = (a * z1 ** 2 + 3 * x1 ** 2) % p
+            b_b = (y1 * z1) % p
+            b_c = (x1 * y1 * b_b) % p
+            b_d = (b_a ** 2 - 8 * b_c) % p
+
+            x3 = (2 * b_b * b_d) % p
+            y3 = (b_a * (4 * b_c - b_d) - 8 * y1 ** 2 * b_b ** 2) % p
+            z3 = (8 * b_b ** 3) % p
+
+            return PointXYZ(x3, y3, z3)
+
+    def to_xy(self):
+        if self.z == 0:
+            return xy_neutral
+        else:
+            z_inv = mod_inv(self.z, p)
+            new_x = self.x * z_inv
+            new_y = self.y * z_inv
+            return PointXY(new_x, new_y)
+
+
+xyz_neutral = PointXYZ(0, 1, 0)
+xy_neutral = PointXY(0, 0)
+
+p = 0
+a = 0
+b = 0
+
+def on_short_weierstrass(point: PointXY):
+    x1 = point.x
+    y1 = point.y
+
+    left = (y1**2) % p
+    right = (x1**3 + a*x1 + b) % p
+
+    return left == right
+
+def on_homogenised_weierstrass(point: PointXYZ):
+    x1 = point.x
+    y1 = point.y
+    z1 = point.z
+
+    left = (y1**2 * z1) % p
+    right = (x1**3 + a * x1 * z1**2 + b * z1**3) % p
+
+    return left == right
+
+def skalarmul(point: PointXY, k):
+    point = point.to_xyz()
+    q = xyz_neutral
+    j = len(k)-1
+    for i in range(j, -1, -1):
+        q = q.dbl()
+        if k[i] == '1':
+            q = q + point
+    return q.to_xy()
+
+def mod_inv(k, mod):
+    if k == 0:
+        raise ZeroDivisionError('Division durch Null')
+    return pow(k, -1, mod)
+
+
+"""
+main() Methode
+"""
+if __name__ == "__main__":
+    # Y^2*Z = X^3 + a*X*Z^2 + b*Z^3
+    # y^2 = x^3 + a*X + b
+    p = int(0xA9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377)
+    a = int(0x7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9)
+    b = int(0x26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6)
+
+    point_p = PointXYZ(60306380415904663168568911239273826053144841234228559299517684417361346433053,
+                 74653857005150983469598545140707432309023702960881435319026826228339031179596, 1)
+    p_val = on_homogenised_weierstrass(point_p)
+    p2 = point_p.dbl()
+    p2_val = on_homogenised_weierstrass(p2)
+    p3 = point_p + p2
+    p3_val = on_homogenised_weierstrass(p3)
+    print(f'point: {point_p}, valid: {p_val}', f'p2 {p2}, valid: {p2_val}', f'p3 {p3}, valid: {p3_val}', f'', sep='\n')
+
+    point_p = PointXY(60306380415904663168568911239273826053144841234228559299517684417361346433053,
+                 74653857005150983469598545140707432309023702960881435319026826228339031179596)
+    p_val = on_short_weierstrass(point_p)
+    p2 = point_p.dbl()
+    p2_val = on_short_weierstrass(p2)
+    p3 = point_p + p2
+    p3_val = on_short_weierstrass(p3)
+    print(f'point: {point_p}, valid: {p_val}', f'p2 {p2}, valid: {p2_val}', f'p3 {p3}, valid: {p3_val}', f'', sep='\n')
+
+    #DH
+    Gf = PointXY(63243729749562333355292243550312970334778175571054726587095381623627144114786,
+                 38218615093753523893122277964030810387585405539772602581557831887485717997975)
+    x = 13
+    y = 21
+    xG = Gf * x
+    yG = Gf * y
+    y_xG = xG * y
+    x_yG = yG * x
+    print(f'Gf: {Gf}, {on_short_weierstrass(Gf)}',
+          f'xG: {xG}, {on_short_weierstrass(xG)}', f'yG: {yG}, {on_short_weierstrass(yG)}',
+          f'y_xG: {y_xG}, {on_short_weierstrass(y_xG)}', f'x_yG: {x_yG}, {on_short_weierstrass(x_yG)}',
+          f'{y_xG == x_yG}', f'xyG == yxG: {y_xG == x_yG}', sep='\n')

76fefba79fb34ceae0930e962f9be3b313d59644
Jonas Kawohl <j.kawohl@icloud.com>
2025-06-06 11:31:42 +0200

    update

--- python/ak2/ecc/MyMath.py
+++ python/ak2/ecc/MyMath.py
@@ -1,4 +1,4 @@
 def mod_inv(k, mod):
     if k == 0:
-        raise ZeroDivisionError('Division durch Null')
+        return 0 % mod
     return pow(k, -1, mod)
--- python/ak2/ecc/Vorlesung.py
+++ python/ak2/ecc/Vorlesung.py
@@ -26,20 +26,22 @@ if __name__ == "__main__":
 
     d1 = int(input('d1: '))
     print(f'{d1} mod {g_mod} = {d1 % g_mod}')
-    p1 = g2 * d1
+    md1 = d1 % g_mod
+    p1 = gen_point(g2, md1)
     print(f'Team 1: P{p1}, on_curve: {p1.on_curve()}', f'', sep='\n')
 
     d2 = int(input('d2: '))
     print(f'{d2} mod {g_mod} = {d2 % g_mod}')
-    p2 = g2 * d2
+    md2 = d2 % g_mod
+    p2 = gen_point(g2, md2)
     print(f'Team 2: P{p2}, on_curve: {p2.on_curve()}', f'', sep='\n')
 
-    secret_p1 = gen_point(p2, d1)
-    secret_p2 = gen_point(p1, d2)
+    secret_p1 = gen_point(p2, md1)
+    secret_p2 = gen_point(p1, md2)
     d = d1 * d2
     print(f'd1 * d2 = {d} mod {g_mod} = {d % g_mod}')
-    d = d % g_mod
-    secret = g2 * d
+    md = d % g_mod
+    secret = g2 * md
 
     print(f'Secret 1: {secret_p1}, {secret_p1.on_curve()}', sep='\n')
     print(f'Secret 2: {secret_p2}, {secret_p2.on_curve()}', sep='\n')

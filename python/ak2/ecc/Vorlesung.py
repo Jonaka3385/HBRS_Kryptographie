5111038a1b35a3efaf340c4130e71a42cd7c1f9c
Jonas Kawohl <j.kawohl@icloud.com>
2025-06-06 11:20:49 +0200

    update

--- python/ak2/ecc/Vorlesung.py
+++ python/ak2/ecc/Vorlesung.py
@@ -21,18 +21,25 @@ if __name__ == "__main__":
     p = 31
     curve = WeierstrassCurve(a, b, p)
     g2 = PointXY(25, 24, curve)
+    g_mod = 7
+    print(f'Generator: {g2}', f'Generator Modulo: {g_mod}', f'', sep='\n')
 
     d1 = int(input('d1: '))
+    print(f'{d1} mod {g_mod} = {d1 % g_mod}')
     p1 = g2 * d1
     print(f'Team 1: P{p1}, on_curve: {p1.on_curve()}', f'', sep='\n')
 
     d2 = int(input('d2: '))
+    print(f'{d2} mod {g_mod} = {d2 % g_mod}')
     p2 = g2 * d2
     print(f'Team 2: P{p2}, on_curve: {p2.on_curve()}', f'', sep='\n')
 
     secret_p1 = gen_point(p2, d1)
     secret_p2 = gen_point(p1, d2)
-    secret = g2 * (d1 * d2)
+    d = d1 * d2
+    print(f'd1 * d2 = {d} mod {g_mod} = {d % g_mod}')
+    d = d % g_mod
+    secret = g2 * d
 
     print(f'Secret 1: {secret_p1}, {secret_p1.on_curve()}', sep='\n')
     print(f'Secret 2: {secret_p2}, {secret_p2.on_curve()}', sep='\n')
